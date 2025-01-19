e9cd1710bf442c46ca01504e9ed71d21335ab74f
Sandun Induranga <sandutharu40@gmail.com>
2025-01-19 13:32:42 +0530

    Weather Page Created

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeatherappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    WeatherPage()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeatherappTheme {
-        Greeting("Android")
     }
 }
