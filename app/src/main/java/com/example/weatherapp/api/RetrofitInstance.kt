0977e1d125b96d9024a1040dcdf4a83945d3bc50
Sandun Induranga <sandutharu40@gmail.com>
2025-03-16 20:29:59 +0530

    Implement weather data retrieval in WeatherViewModel and add API key constant

--- app/src/main/java/com/example/weatherapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/weatherapp/api/RetrofitInstance.kt
@@ -6,11 +6,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
 
     private const val BASE_URL = "https://api.weatherapi.com"
-//    https://api.weatherapi.com/v1/current.json?key=14480298fb3e40138f3144338251603&q=London&aqi=no
+
     private fun getRetrofitInstance(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
+
+    val weatherApi: WeatherApi = getRetrofitInstance().create(WeatherApi::class.java)
+
 }
--- app/src/main/java/com/example/weatherapp/models/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/models/WeatherViewModel.kt
@@ -2,9 +2,24 @@ package com.example.weatherapp.models
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.api.Constants
+import com.example.weatherapp.api.RetrofitInstance
+import kotlinx.coroutines.launch
 
-class WeatherViewModel: ViewModel() {
-    fun getWeatherData(city: String){
-        Log.d("WeatherViewModel", "City: $city")
+class WeatherViewModel : ViewModel() {
+
+    private val weatherApi = RetrofitInstance.weatherApi
+
+    fun getWeatherData(city: String) {
+        viewModelScope.launch {
+            val response = weatherApi.getWeatherData(Constants.API_KEY, city)
+            if (response.isSuccessful) {
+                val weatherResponse = response.body()
+                Log.d("WeatherViewModel", "getWeatherData: $weatherResponse")
+            } else {
+                Log.d("WeatherViewModel", "getWeatherData: ${response.errorBody()}")
+            }
+        }
     }
 }
