45b32e897b3f2e2fb8ce6792716a0266fe68396d
Sandun Induranga <sandutharu40@gmail.com>
2025-03-16 20:56:10 +0530

    Refactor WeatherViewModel to handle network responses with LiveData and improve error handling

--- app/src/main/java/com/example/weatherapp/models/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/models/WeatherViewModel.kt
@@ -1,24 +1,37 @@
 package com.example.weatherapp.models
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.api.Constants
+import com.example.weatherapp.api.NetworkResponse
 import com.example.weatherapp.api.RetrofitInstance
 import kotlinx.coroutines.launch
 
 class WeatherViewModel : ViewModel() {
 
     private val weatherApi = RetrofitInstance.weatherApi
+    private val _weatherResult = MutableLiveData<NetworkResponse<WeatherResponse>>()
+    private val weatherResult: LiveData<NetworkResponse<WeatherResponse>> = _weatherResult
 
     fun getWeatherData(city: String) {
+        _weatherResult.value = NetworkResponse.Loading
         viewModelScope.launch {
-            val response = weatherApi.getWeatherData(Constants.API_KEY, city)
-            if (response.isSuccessful) {
-//                val weatherResponse = response.body()
-                Log.d("WeatherViewModel", "getWeatherData: ${response.body().toString()}")
-            } else {
-                Log.d("WeatherViewModel", "getWeatherData: ${response.errorBody()}")
+            try {
+                val response = weatherApi.getWeatherData(Constants.API_KEY, city)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        _weatherResult.value = NetworkResponse.Success(it)
+                    }
+                } else {
+                    _weatherResult.value = NetworkResponse.Error("Failed to fetch data")
+                }
+
+            } catch (e: Exception) {
+                _weatherResult.value = NetworkResponse.Error("Failed to fetch data")
+                Log.e("WeatherViewModel", "getWeatherData: ${e.message}")
             }
         }
     }
