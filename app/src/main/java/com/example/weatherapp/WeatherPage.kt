8685740db2bd90460a4dfc45cfa92cc90494eab9
Sandun Induranga <sandutharu40@gmail.com>
2025-03-16 21:15:19 +0530

    Enhance WeatherPage layout with additional weather details and improved text styling

--- app/src/main/java/com/example/weatherapp/WeatherPage.kt
+++ app/src/main/java/com/example/weatherapp/WeatherPage.kt
@@ -3,7 +3,9 @@ package com.example.weatherapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -22,7 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.weatherapp.api.NetworkResponse
 import com.example.weatherapp.models.WeatherResponse
 import com.example.weatherapp.models.WeatherViewModel
@@ -106,8 +112,19 @@ fun WeatherDetails(data: WeatherResponse) {
                     .padding(end = 4.dp)
                     .size(40.dp)
             )
-            Text(text = data.location.name)
+            Text(text = data.location.name, fontSize = 30.sp)
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(text = data.location.country, fontSize = 18.sp, color = Color.Gray)
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Temperature: ${data.current.temp_c}°C",
+            fontSize = 56.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
     }
 
 }
